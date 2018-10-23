0612d87a0865dda455494ddac0fcb18e5dd223f8
Sanket Singh <singhsanket143@gmail.com>
2018-10-23 15:43:13 +0530

    Added sockets

---
+++ server.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+var pythonPort = 5000;
+var serverAddress = 'localhost';
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log("New user connected");
+
+    socket.on('disconnect', () => {
+        console.log("User disconnected");
+    });
+});
+
+
+
+server.listen(port, () => {
+    console.log(`Server is up on ${port}`);
+});
